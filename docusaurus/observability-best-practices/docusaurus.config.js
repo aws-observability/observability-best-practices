81128f683b9155245d469c4fa842470a9c1fc856
Siva Guruvareddiar <gurusiva@amazon.com>
2024-09-20 16:53:16 -0500

    changes

--- docusaurus/observability-best-practices/docusaurus.config.js
+++ docusaurus/observability-best-practices/docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'AWS Observability best practices',
-  tagline: '🖥️ AWS Insights: Elevate Your Cloud with Smart Observability 🚀',
+  tagline: '🖥️ AWS Insights: Improving Cloud Observability 🚀',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
